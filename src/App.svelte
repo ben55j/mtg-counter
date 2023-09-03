26e93e6edfe021cc4d99298e5b247c0780ab4eb8
Bazogui-lbm <bazogui.ext@lebonmarche.fr>
2023-09-04 00:13:33 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Team from "./Team.svelte";
+</script>
+
+<div class="row mt-2">
+  <div class="col">
+   <h1 class="text-center">MTG Counter</h1>
+  </div>
+</div>
+
+<div class="row">
+  <Team name="Red"/>
+  <Team name="Blue"/>
+</div>
---
+++ src/Team.svelte
@@ -0,0 +1,30 @@
+<script>
+    let score =25;
+    export  let name ="red";
+
+    function increase(){
+        score++;
+    }
+    function decrease(){
+        score--;
+    }
+</script>
+
+<div style="border:dashed red 5px" class="col">
+
+    <h2 class="text-danger">Team {name}: {score}</h2>
+    <button on:click={increase} class="btn btn-success">+</button>
+    <button on:click={decrease} class="btn btn-danger">-</button>
+
+</div>
+
+
+<style>
+
+    div.col{
+
+     padding: 20px;
+     margin: 15px;
+     text-align: center;
+    }
+</style>
